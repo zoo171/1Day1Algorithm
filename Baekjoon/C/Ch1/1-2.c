5de71dd9360c7f9dd832a7571f4d781e49d1aaff
Song Juhun <gathers2001@gmail.com>
2025-04-30 16:29:35 +0900

    1-2 백준에 맞게 수정정

--- Baekjoon/C/Ch1/1-2.c
+++ Baekjoon/C/Ch1/1-2.c
@@ -8,7 +8,7 @@ int main()
     int B = 0;
 
     scanf("%d %d", &A, &B);
-    printf("%d + %d = %d", A, B, A+B);
+    printf("%d", A+B);
 
     return 0;
 }
--- Baekjoon/python/Ch1/1-2.py
+++ Baekjoon/python/Ch1/1-2.py
@@ -1,6 +1,4 @@
 # 입출력과 사칙연산
 # 1000번 문제
-A = input()
-B = input()
-C = int(A) + int(B)
-print(C)
+A, B = map(int, input().split())
+print(A + B)
